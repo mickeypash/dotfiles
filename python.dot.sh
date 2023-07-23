518e1d7dffbeef8b5070483f8ed9357c1826221f
mickeypash <pashov.m@gmail.com>
2023-07-23 17:41:39 +0100

    [python] add conda command

--- python.dot.sh
+++ python.dot.sh
@@ -17,11 +17,28 @@ workon() {
   [ -z "$PROJECT_HOME" ] && {
     unset -f workon;
     #export PROJECT_HOME=/server/python/;
+    export VIRTUALENVWRAPPER_PYTHON=$(which python3)
     source /usr/local/bin/virtualenvwrapper.sh
     }
   workon "$@"
 }
 
+,conda() {
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('/Users/pashom2/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/Users/pashom2/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+            . "/Users/pashom2/opt/anaconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/Users/pashom2/opt/anaconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    # <<< conda initialize <<<
+}
 
 function ,pypath {
     # usage: ,pypath [--prepend] [<directories>]
